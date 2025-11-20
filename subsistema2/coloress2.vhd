5e5f786a048f95507edcbac45317f9ac19c7938c
Adrian <perrettaadrianu@gmail.com>
2025-11-20 00:51:02 -0300

    corregido leds

--- subsistema2/coloress2.vhd
+++ subsistema2/coloress2.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity coloress2 is
-	port(SC1 : in std_logic_vector(2 downto 0);
-		  SC2 : in std_logic_vector(2 downto 0);
+	port(SC1 : in integer range 0 to 7;
+		  SC2 : integer range 0 to 7;
 		  INICIO : in std_logic;
 		  MV : in std_logic;
 		  MINV : in std_logic;
@@ -20,7 +20,7 @@ end coloress2;
 
 architecture fnc of coloress2 is
 
-signal CJ2 ,CJ1 : std_logic_vector(23 downto 0);
+signal CJ2, CJ1, D16, D15, D14, D13, D12, D11, D10, D9, D8, D7, D6, D5, D4, D3, D2, D1 : std_logic_vector(23 downto 0);
 signal victoriacounter1, victoriacounter2, empatecounter1, empatecounter2, empategeneral : integer range 1 to 4 := 1;
 signal bloqueo : std_logic := '0';
 begin
@@ -29,21 +29,21 @@ process (SC1)
 	begin
 		if INICIO = '0' then
 			case SC1 is
-				when "000" => 
+				when 0 => 
 					CJ1 <= x"7008E7";
-				when "001" => 
+				when 1 => 
 					CJ1 <= x"401577";
-				when "010" => 
+				when 2 => 
 					CJ1 <= x"8C3BCC";
-				when "011" => 
+				when 3 => 
 					CJ1 <= x"5A158E";
-				when "100" => 
+				when 4 => 
 					CJ1 <= x"6821CB";
-				when "101" => 
+				when 5 => 
 					CJ1 <= x"8A0194";
-				when "110" => 
+				when 6 => 
 					CJ1 <= x"51157D";
-				when "111" => 
+				when 7 => 
 					CJ1 <= x"2F0D68";
 				when others =>
 					null;
@@ -55,21 +55,21 @@ process (SC2)
 	begin
 		if INICIO = '0' then
 			case SC2 is
-				when "000" => 
+				when 0 => 
 					CJ2 <= x"FFDF20";
-				when "001" => 
+				when 1 => 
 					CJ2 <= x"CCB222";
-				when "010" => 
+				when 2 => 
 					CJ2 <= x"E3B340";
-				when "011" => 
+				when 3 => 
 					CJ2 <= x"FF8904";
-				when "100" => 
+				when 4 => 
 					CJ2 <= x"A94619";
-				when "101" => 
+				when 5 => 
 					CJ2 <= x"E1712B";
-				when "110" => 
+				when 6 => 
 					CJ2 <= x"B36E2B";
-				when "111" => 
+				when 7 => 
 					CJ2 <= x"FEE685";
 				when others =>
 					null;
@@ -82,102 +82,102 @@ begin
 if rising_edge(CLK) then
 	if INICIO = '1' then                                             --INICIO TURNO
 		if TURNO = '1' then
-			MATRIZ(23 downto 0) <= CJ1;
+			D1 <= CJ1;
 		else 
-			MATRIZ(23 downto 0) <= CJ2;
+			D1 <= CJ2;
 		end if;
 		if MINV = '1' then
-			MATRIZ(23 downto 0) <= x"FFFFFF";
+			D1 <= x"FFFFFF";
 		end if;                                                       -- FIN TURNO
 		
 		if MV = '1' then                                              --INICIO 3x3
 			case Posicion is
 				when "0000" =>
 					if TURNO = '1' then
-						MATRIZ(383 downto 360) <= CJ1;
+						D16 <= CJ1;
 					else
-						MATRIZ(383 downto 360) <= CJ2;
+						D16 <= CJ2;
 					end if;
 				when "0001" =>
 					if TURNO = '1' then
-						MATRIZ(359 downto 336) <= CJ1;
+						D12 <= CJ1;
 					else
-						MATRIZ(359 downto 336) <= CJ2;
+						D12 <= CJ2;
 					end if;
 				when "0010" =>
 					if TURNO = '1' then
-						MATRIZ(335 downto 312) <= CJ1;
+						D8 <= CJ1;
 					else
-						MATRIZ(335 downto 312) <= CJ2;
+						D8 <= CJ2;
 					end if;
 				when "0011" =>
 					if TURNO = '1' then
-						MATRIZ(287 downto 264) <= CJ1;
+						D15 <= CJ1;
 					else
-						MATRIZ(287 downto 264) <= CJ2;
+						D15 <= CJ2;
 					end if;
 				when "0100" =>
 					if TURNO = '1' then
-						MATRIZ(263 downto 240) <= CJ1;
+						D11 <= CJ1;
 					else
-						MATRIZ(263 downto 240) <= CJ2;
+						D11 <= CJ2;
 					end if;
 				when "0101" =>
 					if TURNO = '1' then
-						MATRIZ(239 downto 216) <= CJ1;
+						D7 <= CJ1;
 					else
-						MATRIZ(239 downto 216) <= CJ2;
+						D7 <= CJ2;
 					end if;
 				when "0110" =>
 					if TURNO = '1' then
-						MATRIZ(191 downto 168) <= CJ1;
+						D14 <= CJ1;
 					else
-						MATRIZ(191 downto 168) <= CJ2;
+						D14 <= CJ2;
 					end if;
 				when "0111" =>
 					if TURNO = '1' then
-						MATRIZ(167 downto 144) <= CJ1;
+						D10 <= CJ1;
 					else
-						MATRIZ(167 downto 144) <= CJ2;
+						D10 <= CJ2;
 					end if;
 				when "1000" =>
 					if TURNO = '1' then
-						MATRIZ(143 downto 120) <= CJ1;
+						D6 <= CJ1;
 					else
-						MATRIZ(143 downto 120) <= CJ2;
+						D6 <= CJ2;
 					end if;
 				when others =>
 					null;
 			end case;
 	end if;
 	else
-		MATRIZ(383 downto 360) <= x"000000";
-		MATRIZ(359 downto 336) <= x"000000";
-		MATRIZ(335 downto 312) <= x"000000";
-		MATRIZ(287 downto 264) <= x"000000";
-		MATRIZ(263 downto 240) <= x"000000";
-		MATRIZ(239 downto 216) <= x"000000";
-		MATRIZ(191 downto 168) <= x"000000";
-		MATRIZ(167 downto 144) <= x"000000";
-		MATRIZ(143 downto 120) <= x"000000";
+		D16 <= x"000000";
+		D12 <= x"000000";
+		D8 <= x"000000";
+		D15 <= x"000000";
+		D11 <= x"000000";
+		D7 <= x"000000";
+		D14 <= x"000000";
+		D10 <= x"000000";
+		D6 <= x"000000";
 	end if;                                                                  -- FIN  3x3
 	
 
 	if INICIO = '0' then                                                     --INICIO PUNTUACION JUGADORES
-		MATRIZ(311 downto 288) <= CJ1;
-		MATRIZ(215 downto 192) <= CJ1;
-		MATRIZ(119 downto 96) <= CJ1;
-		MATRIZ(95 downto 72) <= CJ2;
-		MATRIZ(71 downto 48) <= CJ2;
-		MATRIZ(47 downto 24) <= CJ2;
+		D4 <= CJ1;
+		D3 <= CJ1;
+		D2 <= CJ1;
+		D13 <= CJ2;
+		D9 <= CJ2;
+		D5 <= CJ2;
 	else
 		if bloqueo = '0' then
-			MATRIZ(311 downto 288) <= x"000000";
-			MATRIZ(215 downto 192) <= x"000000";
-			MATRIZ(119 downto 96) <= x"000000";
-			MATRIZ(95 downto 72) <= x"000000";
-			MATRIZ(71 downto 48) <= x"000000";
-			MATRIZ(47 downto 24) <= x"000000";
+			D4 <= x"000000";
+			D3 <= x"000000";
+			D2 <= x"000000";
+			D13 <= x"000000";
+			D9 <= x"000000";
+			D5 <= x"000000";
 			bloqueo <= '1';
 		end if;
 	end if;
@@ -185,11 +185,11 @@ if rising_edge(CLK) then
 		if TURNO = '1' then
 			case victoriacounter1 is
 				when 1 =>
-					MATRIZ(311 downto 288) <= CJ1;
+					D4 <= CJ1;
 				when 2 =>
-					MATRIZ(215 downto 192) <= CJ1;
+					D3 <= CJ1;
 				when 3 =>
-					MATRIZ(119 downto 96) <= CJ1;
+					D2 <= CJ1;
 				when 4 =>
 					null;
 			end case;
@@ -199,11 +199,11 @@ if rising_edge(CLK) then
 		if TURNO = '0' then
 			case victoriacounter2 is
 				when 1 =>
-					MATRIZ(95 downto 72) <= CJ2;
+					D13 <= CJ2;
 				when 2 =>
-					MATRIZ(71 downto 48) <= CJ2;
+					D9 <= CJ2;
 				when 3 =>
-					MATRIZ(47 downto 24) <= CJ2;
+					D5 <= CJ2;
 				when 4 =>
 					null;
 			end case;
@@ -215,21 +215,21 @@ if rising_edge(CLK) then
 	if EMPATE = '1' then 
 		case empatecounter1 is
 			when 1 =>
-				MATRIZ(311 downto 288) <= x"FFFFFF";
+				D4 <= x"FFFFFF";
 			when 2 =>
-				MATRIZ(215 downto 192) <= x"FFFFFF";
+				D3 <= x"FFFFFF";
 			when 3 =>
-				MATRIZ(119 downto 96) <= x"FFFFFF";
+				D2 <= x"FFFFFF";
 			when 4 =>
 				null;
 		end case;
 		case empatecounter2 is
 			when 1 =>
-				MATRIZ(95 downto 72) <= x"FFFFFF";
+				D13 <= x"FFFFFF";
 			when 2 =>
-				MATRIZ(71 downto 48) <= x"FFFFFF";
+				D9 <= x"FFFFFF";
 			when 3 =>
-				MATRIZ(47 downto 24) <= x"FFFFFF";
+				D5 <= x"FFFFFF";
 			when 4 =>
 				null;
 		end case;
@@ -240,37 +240,37 @@ if rising_edge(CLK) then
 	   
 	if VF = '1' then                                                     --INICIO VICTORIA FINAL
 		if victoriacounter1 = 4 then
-			MATRIZ(95 downto 72) <= x"004E68";
-			MATRIZ(71 downto 48) <= x"FFFFFF";
-			MATRIZ(47 downto 24) <= x"EF4135";
-			MATRIZ(311 downto 288) <= CJ1;
-			MATRIZ(215 downto 192) <= CJ1;
-			MATRIZ(119 downto 96) <= CJ1;
+			D13 <= x"004E68";
+			D9 <= x"FFFFFF";
+			D5 <= x"EF4135";
+			D4 <= CJ1;
+			D3 <= CJ1;
+			D2 <= CJ1;
 		end if;
 		if victoriacounter2 = 4 then
-			MATRIZ(311 downto 288) <= x"004E68";
-			MATRIZ(215 downto 192) <= x"FFFFFF";
-			MATRIZ(119 downto 96) <= x"EF4135";
-			MATRIZ(95 downto 72) <= CJ2;
-			MATRIZ(71 downto 48) <= CJ2;
-			MATRIZ(47 downto 24) <= CJ2;
+			D4 <= x"004E68";
+			D3 <= x"FFFFFF";
+			D2 <= x"EF4135";
+			D13 <= CJ2;
+			D9 <= CJ2;
+			D5 <= CJ2;
 		end if;
 		if empategeneral = 4 then
-			MATRIZ(95 downto 72) <= x"FFFFFF";
-			MATRIZ(71 downto 48) <= x"FFFFFF";
-			MATRIZ(47 downto 24) <= x"FFFFFF";
-			MATRIZ(311 downto 288) <= x"FFFFFF";
-			MATRIZ(215 downto 192) <= x"FFFFFF";
-			MATRIZ(119 downto 96) <= x"FFFFFF";
-			MATRIZ(383 downto 360) <= x"FFFFFF";
-			MATRIZ(359 downto 336) <= x"FFFFFF";
-			MATRIZ(335 downto 312) <= x"FFFFFF";
-			MATRIZ(287 downto 264) <= x"FFFFFF";
-			MATRIZ(263 downto 240) <= x"FFFFFF";
-			MATRIZ(239 downto 216) <= x"FFFFFF";
-			MATRIZ(191 downto 168) <= x"FFFFFF";
-			MATRIZ(167 downto 144) <= x"FFFFFF";
-			MATRIZ(143 downto 120) <= x"FFFFFF";
+			D13 <= x"FFFFFF";
+			D9 <= x"FFFFFF";
+			D5 <= x"FFFFFF";
+			D4 <= x"FFFFFF";
+			D3 <= x"FFFFFF";
+			D2 <= x"FFFFFF";
+			D16 <= x"FFFFFF";
+			D12 <= x"FFFFFF";
+			D8 <= x"FFFFFF";
+			D15 <= x"FFFFFF";
+			D11 <= x"FFFFFF";
+			D7 <= x"FFFFFF";
+			D14 <= x"FFFFFF";
+			D10 <= x"FFFFFF";
+			D6 <= x"FFFFFF";
 		end if;
 		victoriacounter1 <= 1;
 		victoriacounter2 <= 1;
@@ -281,5 +281,20 @@ if rising_edge(CLK) then
 	end if;                                                                --FIN VICTORIA FINAL
 end if;
 end process;
-		
+MATRIZ(383 downto 360)<=D16;
+MATRIZ(359 downto 336)<=D15;
+MATRIZ(335 downto 312)<=D14;
+MATRIZ(311 downto 288)<=D13;
+MATRIZ(287 downto 264)<=D12;
+MATRIZ(263 downto 240)<=D11;
+MATRIZ(239 downto 216)<=D10;
+MATRIZ(215 downto 192)<=D9;
+MATRIZ(191 downto 168)<=D8;
+MATRIZ(167 downto 144)<=D7;
+MATRIZ(143 downto 120)<=D6;
+MATRIZ(119 downto 96)<=D5;
+MATRIZ(95 downto 72)<=D4;
+MATRIZ(71 downto 48)<=D3;
+MATRIZ(47 downto 24)<=D2;
+MATRIZ(23 downto 0)<=D1;
 end;
